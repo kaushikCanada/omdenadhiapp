9db548e44f58021583c0285b6bfbd1aa46550d93
Kaushik Roy <kaushik.roy1984@gmail.com>
2022-10-04 07:28:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import streamlit as st
 from transformers import pipeline
 
 
+def get_option(option):
+    if option == "Text Summarisation":
+        sentence = st.text_area('Enter your text here...')
+        if sentence:
+            summarization = pipeline("summarization")
+            summary_text = summarization(sentence)[0]['summary_text']
+            st.write(summary_text)
+
+option = st.sidebar.selectbox("Select option",  ("Text Summarisation","Other"))
+
+get_option(option)
