5fcb43ca6c28ea62f279b05054034a4edee8bb57
Kaushik Roy <kaushik.roy1984@gmail.com>
2022-10-04 07:45:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from transformers import pipeline
 
 
 def get_option(option):
-    if option == "Text Summarisation":
+    if option == "Dzongkha to English":
         sentence = st.text_area('Enter your text here...')
         if sentence:
             summarization = pipeline("summarization")
             summary_text = summarization(sentence)[0]['summary_text']
             st.write(summary_text)
 
-option = st.sidebar.selectbox("Select option",  ("Text Summarisation","Other"))
+option = st.sidebar.selectbox("Select option",  ("Dzongkha to English","English to Dzongkha"))
 
 get_option(option)
