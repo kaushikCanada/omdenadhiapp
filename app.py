399278661ad50e5ae23f98977018748942be3669
Kaushik Roy <kaushik.roy1984@gmail.com>
2022-10-08 15:19:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from transformers import pipeline
 
-st.title("Omdena DHI Dzongkha-English Translation Application")
+st.set_page_config(page_title='Omdena DHI Dzongkha-English Translation Application', page_icon=':shark:', layout="centered", initial_sidebar_state="auto", menu_items=None)
+
 
 def get_option(option):
     if option == "Dzongkha to English":
+        st.title("Please enter text in Dzongkha")
         sentence = st.text_area('Enter your text here...')
         if sentence:
             summarization = pipeline("summarization")
